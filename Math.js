282b1c578aab011013f0ba333b6a2bb74a22abc4
wellpinho <wellpinho@outlook.com>
2021-12-17 20:20:19 -0300

    ✨ NEW FEATURE: Maths

---
+++ Math.js
@@ -0,0 +1,20 @@
+// Math API
+Math.abs // converte o sinal do numero para positivo
+Math.ceil // Arredonda o número para cima
+Math.floor // Arredonda o numero para baixo
+Math.round
+/*Arredonda para cima se a parte decimal for de 5 a 9 e
+           para baixo de for de 0 a 4 */
+Math.sign // Retorna 1 se o número for positivo e -1 se for negativo
+Math.trunc // Elimina a parte decimal do número tonando-o inteiro
+
+Math.cbrt // retorna a raiza cubica do numero
+Math.cos // retorna o coseno de um angulo
+Math.hypot // retorna raiza quadrada dos quadrados dos numeros
+Math.pow // retorna numero elevado a um determinado expoente
+Math.sin // retorna o seno de um angulo
+Math.sqrt // retorna a raiz quadrada do numero
+Math.tan // retorna atangente de um numero
+Math.min // retorna o menor numero passado por parametro
+Math.max // retorna o maior numero passado por parametro
+Math.random // retorna um numero randomico entre 0 e 1, não incluindo o 1
